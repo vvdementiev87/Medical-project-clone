cd3559d772f83b30f398b3af35f8a838cc7368a5
Konstantin Pokatilov <kotyara9494@list.ru>
2023-03-04 20:23:06 +1000

    Добавил две таблицы - videos и articles, на них же сидеры. Передаем данные на фронт

---
+++ backend/app/Http/Controllers/Content/ArticlesController.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\Content;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Articles as ArticlesModel;
+
+
+class ArticlesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): string
+    {
+        $articles = new ArticlesModel();
+        $result;
+        foreach ($articles->get() as $item) {
+            $result[$item->id] = [
+                'author' => $item->author,
+                'title' => $item->title,
+                'description' => $item->description,
+                'imageUrl' => $item->image_url,
+                'shortText' => $item->short_text,
+                'textHTML' => $item->text_html
+            ];
+        }
+        return json_encode($result);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ backend/database/migrations/2023_03_04_091448_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('author');
+            $table->string('title');
+            $table->string('description');
+            $table->string('image_url');
+            $table->string('short_text');
+            $table->string('text_html');
+            $table->timestamps();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
