64a9c913c9e1aa1e1ce3d6c2b0dcb33ea740ece4
Yuriy <yfrolou@mail.ru>
2023-03-18 19:30:59 +0300

    created roundedButton and push to topSection

--- front/src/components/TopSection/TopSection.jsx
+++ front/src/components/TopSection/TopSection.jsx
@@ -4,23 +4,24 @@ import topFoto from '../../assets/testImages/Top.png';
 import { Parallax } from 'react-parallax';
 import NewsBanner from "../NewsBanner/NewsBanner";
 import {useNavigate} from "react-router-dom";
+import RoundedButton from "../../ui/rounded-button/RoundedButton";
 
-const news={
-	id: 1,
-		title:
-	'Продлено сотрудничество с Обществом симуляции в здравоохранении (SSH).',
-		imageUrl: '/imagesTest/news_1.jpg',
-	shortText:
-	'24 января 2023 года в Орландо (США) в рамках работы Международной конференции по симуляции в здравоохранении (IMSH-2023) было продлено соглашение о сотрудничестве между Российским обществом симуляционно...',
-		bigText:
-	'24 января 2023 года в Орландо (США) в рамках работы Международной конференции по симуляции в здравоохранении (IMSH-2023) было продлено соглашение о сотрудничестве между  Российским обществом симуляционного обучения (РОСОМЕД)  и Обществом симуляции в здравоохранении (SSH). Соглашение подписали Председатель президиума правления РОСОМЕД, Александр Колыш, и Президент SSH, Хару Окуда. Надеемся на эффективное продолжение многолетнего сотрудничества!',
-};
+// const news={
+// 	id: 1,
+// 		title:
+// 	'Продлено сотрудничество с Обществом симуляции в здравоохранении (SSH).',
+// 		imageUrl: '/imagesTest/news_1.jpg',
+// 	shortText:
+// 	'24 января 2023 года в Орландо (США) в рамках работы Международной конференции по симуляции в здравоохранении (IMSH-2023) было продлено соглашение о сотрудничестве между Российским обществом симуляционно...',
+// 		bigText:
+// 	'24 января 2023 года в Орландо (США) в рамках работы Международной конференции по симуляции в здравоохранении (IMSH-2023) было продлено соглашение о сотрудничестве между  Российским обществом симуляционного обучения (РОСОМЕД)  и Обществом симуляции в здравоохранении (SSH). Соглашение подписали Председатель президиума правления РОСОМЕД, Александр Колыш, и Президент SSH, Хару Окуда. Надеемся на эффективное продолжение многолетнего сотрудничества!',
+// };
 
 const TopSection = () => {
-	const [announcement,setAnnouncement]=useState();
-	useEffect(()=>{
-		setAnnouncement(news?.shortText);
-	},[])
+	// const [announcement,setAnnouncement]=useState();
+	// useEffect(()=>{
+	// 	setAnnouncement(news?.shortText);
+	// },[])
 	const navigate = useNavigate();
 	return (
 		<div className={styles.topSection} id="topSection">
@@ -39,24 +40,25 @@ const TopSection = () => {
 								аттестации, внедрение передовых технологий в сфере
 								здравоохранения
 							</p>
-							{news?
-								<button onClick={() => {
-									navigate(`/news/${news.id}`);
-								}}>
-									<NewsBanner text={announcement}/>
-								</button>:""
-							}
+							{/*{news?*/}
+								<RoundedButton onClick={() => {
+									navigate(`/`);
+								}} text={"Подробнее"}>
+									{/*<NewsBanner text={announcement}/>*/}
+								</RoundedButton>
+							{/*:""*/}
+							{/*}*/}
 						</div>
 					</div>
 					<div className={`col-lg p-0 ${styles.topImage}`}>
-						<Parallax
-							blur={10}
-							bgImage={topFoto}
-							bgImageAlt="the cat"
-							strength={200}
-						>
+						{/*<Parallax*/}
+						{/*	blur={10}*/}
+						{/*	bgImage={topFoto}*/}
+						{/*	bgImageAlt="the cat"*/}
+						{/*	strength={200}*/}
+						{/*>*/}
 							<img src={topFoto} alt="Foto" className={`${styles.mainFoto}`} />
-						</Parallax>
+						{/*</Parallax>*/}
 					</div>
 				</div>
 
