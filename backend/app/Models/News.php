2b8d9556212657a0dd2ec838ab9214ec577f015b
Konstantin Pokatilov <kotyara9494@list.ru>
2023-03-19 20:00:16 +1000

    выдача новостей по актуальности

--- backend/app/Models/News.php
+++ backend/app/Models/News.php
@@ -1,15 +1,14 @@
 <?php
- 
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
-class Videos extends Model
+class News extends Model
 {
     use HasFactory, Searchable;
 
-    protected $table = 'videos';
-
+    protected $table = 'news';
 }
