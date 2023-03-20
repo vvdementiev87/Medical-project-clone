429bbccd77f004d7ce71c0a5e2281d76903c093c
vvdementiev87 <vvdementiev87@gmail.com>
2023-03-20 17:49:42 +0300

    Front fix

--- front/src/config/api.config.jsx
+++ front/src/config/api.config.jsx
@@ -1,6 +1,7 @@
 export const API_URL = `${process.env.REACT_APP_URL}/`;
+export const MAILISEARCH_URL = process.env.REACT_APP_MAILISEARCH_URL;
 
 export const getAuthUrl = (string) => `api/auth${string}`;
 export const getVideosUrl = (string) => `api/content/videos${string}`;
-export const getNewsUrl = (string) => `api/content/articles${string}`; //to be changed for news
+export const getNewsUrl = (string) => `api/content/news${string}`; //to be changed for news
 export const getArticlesUrl = (string) => `api/content/articles${string}`;
